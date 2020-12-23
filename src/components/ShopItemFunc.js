610f12952a862368b2520d72e2f50df72019200f
Vadim2107 <58651870+Vadim2107@users.noreply.github.com>
2020-12-23 20:34:10 +0300

    add models

--- src/components/ShopItemFunc.js
+++ src/components/ShopItemFunc.js
@@ -1,15 +1,11 @@
+import React from 'react';
 import '../App.css';
+import PropTypes from 'prop-types';
+import ItemModel from '../models/ItemModel';
 
-function ShopItemFunc() {
-    const item = {
-        brand: 'Tiger of Sweden',
-        title: 'Leonard coat',
-        description: 'Minimalistic coat in cotton-blend',
-        descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
-        price: 399,
-        currency: '£'
-    }
-
+function ShopItemFunc(props) {
+    const {item} = props;
+    
     return (
         <div className="main-content">
             <h2>{item.brand}</h2>
@@ -27,5 +23,9 @@ function ShopItemFunc() {
         </div>
     );
 }
+
+ShopItemFunc.propTypes = {
+    item: PropTypes.instanceOf(ItemModel).isRequired
+    }
 
 export default ShopItemFunc;
